ae25cdb56a7cdacce8a66463a9783efdacd7e9dc
Kelsey Mwongeli <kelseykm.git@gmail.com>
2022-09-05 01:51:29 +0300

    feat: add verify files existence
    also:
      - remove blinking on "error"
      - refactor some lines

--- argparse/argparse.go
+++ argparse/argparse.go
@@ -12,15 +12,35 @@ var timeTakenFlag bool
 var encryptFlag bool
 var decryptFlag bool
 
+func verifyFiles(files []string) (filesExist bool, fakeFiles []string) {
+	filesExist = true
+
+	for _, file := range files {
+		openFile, err := os.Open(file)
+		if err != nil {
+			filesExist = false
+			fakeFiles = append(fakeFiles, file)
+		}
+		openFile.Close()
+	}
+
+	return
+}
+
 // ParseFlags parses flags and arguments and  returns them
 func ParseFlags() (timeTaken, encrypt, decrypt bool, files []string) {
 	flag.Parse()
+
+	timeTaken = timeTakenFlag
+	encrypt = encryptFlag
+	decrypt = decryptFlag
+	files = flag.Args()
+
 	var errorString string
 
-	if len(flag.Args()) == 0 {
+	if len(files) == 0 {
 		errorString = fmt.Sprintf(
-			"%s%serror%s %sfile(s) to be decrypted/encrypted not provided%s\n\n",
-			colour.RedBlinking,
+			"%serror:%s %sfile(s) to be decrypted/encrypted not provided%s\n\n",
 			colour.RedBold,
 			colour.Normal,
 			colour.WhiteBold,
@@ -31,8 +51,7 @@ func ParseFlags() (timeTaken, encrypt, decrypt bool, files []string) {
 		os.Exit(2)
 	} else if encryptFlag && decryptFlag {
 		errorString = fmt.Sprintf(
-			"%s%serror%s %scannot set both -decrypt and -encrypt%s\n\n",
-			colour.RedBlinking,
+			"%serror:%s %scannot set both -decrypt and -encrypt%s\n\n",
 			colour.RedBold,
 			colour.Normal,
 			colour.WhiteBold,
@@ -43,8 +62,7 @@ func ParseFlags() (timeTaken, encrypt, decrypt bool, files []string) {
 		os.Exit(2)
 	} else if !encryptFlag && !decryptFlag {
 		errorString = fmt.Sprintf(
-			"%s%serror%s %seither -decrypt or -encrypt must be set%s\n\n",
-			colour.RedBlinking,
+			"%serror:%s %seither -decrypt or -encrypt must be set%s\n\n",
 			colour.RedBold,
 			colour.Normal,
 			colour.WhiteBold,
@@ -55,17 +73,34 @@ func ParseFlags() (timeTaken, encrypt, decrypt bool, files []string) {
 		os.Exit(2)
 	}
 
-	timeTaken = timeTakenFlag
-	encrypt = encryptFlag
-	decrypt = decryptFlag
-	files = flag.Args()
+	filesOk, badFiles := verifyFiles(files)
+	if !filesOk {
+		errorString = fmt.Sprintf(
+			"%serror:%s %ssome files do not exist:%s\n",
+			colour.RedBold,
+			colour.Normal,
+			colour.WhiteBold,
+			colour.Normal,
+		)
+		fmt.Fprintf(flag.CommandLine.Output(), errorString)
+
+		for index, badFile := range badFiles {
+			fmt.Printf(
+				"  %v: %s%s%s\n",
+				index+1,
+				colour.BrownItalicised,
+				badFile,
+				colour.Normal,
+			)
+		}
+	}
 
 	return
 }
 
 func init() {
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "usage: kelcryptor [-h|-help] [[-e|-encrypt]|[-d|-decrypt]] [-time] FILE [FILE ...]\n\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "usage: kelcryptor [-h|-help] [[-e|-encrypt]|[-d|-decrypt]] [-t|-time] FILE [FILE ...]\n\n")
 		flag.PrintDefaults()
 	}
 
