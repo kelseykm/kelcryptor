3d07a0f5d9f8a836f4c138ef060502ba5b2d606e
Kelsey Mwongeli <kelseykm.git@gmail.com>
2022-09-04 17:04:21 +0300

    feat: add colour module

---
+++ colour/colour.go
@@ -0,0 +1,46 @@
+package colour
+
+// Exported colours
+const (
+	Red   = "\033[31m"
+	Green = "\033[32m"
+	Brown = "\033[33m"
+	Blue  = "\033[34m"
+	White = "\033[39m"
+
+	RedBold   = "\033[1;31m"
+	GreenBold = "\033[1;32m"
+	BrownBold = "\033[1;33m"
+	BlueBold  = "\033[1;34m"
+	WhiteBold = "\033[1;39m"
+
+	RedUnderlined   = "\033[4;31m"
+	GreenUnderlined = "\033[4;32m"
+	BrownUnderlined = "\033[4;33m"
+	BlueUnderlined  = "\033[4;34m"
+	WhiteUnderlined = "\033[4;39m"
+
+	RedItalicised   = "\033[3;31m"
+	GreenItalicised = "\033[3;32m"
+	BrownItalicised = "\033[3;33m"
+	BlueItalicised  = "\033[3;34m"
+	WhiteItalicised = "\033[3;39m"
+
+	RedStrikeThrough   = "\033[9;31m"
+	GreenStrikeThrough = "\033[9;32m"
+	BrownStrikeThrough = "\033[9;33m"
+	BlueStrikeThrough  = "\033[9;34m"
+	WhiteStrikeThrough = "\033[9;39m"
+
+	RedBackground   = "\033[7;31m"
+	GreenBackground = "\033[7;32m"
+	BrownBackground = "\033[7;33m"
+	BlueBackground  = "\033[7;34m"
+	WhiteBackground = "\033[7;39m"
+
+	RedBlinking   = "\033[5;31m"
+	GreenBlinking = "\033[5;32m"
+	BrownBlinking = "\033[5;33m"
+	BlueBlinking  = "\033[5;34m"
+	WhiteBlinking = "\033[5;39m"
+)
