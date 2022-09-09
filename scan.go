5c0ee5db6df057b95326f222105901f5a74309d4
Kelsey Mwongeli <kelseykm.git@gmail.com>
2022-09-09 18:53:46 +0300

    feat: make password entry invisible

--- scan.go
+++ scan.go
@@ -18,11 +18,15 @@ func scanPassword() (string, error) {
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Printf("%vEnter password: %s", colour.GreenBold, colour.Normal)
+	fmt.Printf("%v", colour.Invisible)
 	password, err := reader.ReadString('\n')
+	fmt.Printf("%v", colour.Normal)
 	checkErr(err)
 
 	fmt.Printf("%vRepeat password: %s", colour.GreenBold, colour.Normal)
+	fmt.Printf("%v", colour.Invisible)
 	passwordConfirm, err := reader.ReadString('\n')
+	fmt.Printf("%v", colour.Normal)
 	checkErr(err)
 
 	if password != passwordConfirm {
